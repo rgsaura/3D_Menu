13aae27d27067c46e434f26ba01b3586c393799e
Rodrigo Garcia <16281075+rgsaura@users.noreply.github.com>
2023-02-05 13:29:31 +0100

    multiplatarforma

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,15 @@ import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
+const isMobile = window.innerWidth <= 700
+
 root.render(
     <Canvas
         camera={ {
             fov: 50,
             near: 0.1,
             far: 2000,
-            position: [ 0, 1, 3.2 ]
+            position: isMobile ? [0, 1, 7] : [0, 1, 3.2]
         } }
     >
         <Experience />
